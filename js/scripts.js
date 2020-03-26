7d26840ef7a67a9e61a6d21a77b002a929f141c7
Tyler Bates <Fondlytyler@gmail.com>
2020-03-26 09:53:23 -0700

    website now displays sections upon clicking submit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $("form#formGroup1").submit(function(event) {
+    event.preventDefault();
+      var selector = $("#selector").val();
+      if(selector === "turtle") {
+        $()
+        $('#turtles').show();
+      } else if(selector === "snake") {
+        $('#snakes').show();
+      } else if(selector === "insect") {
+        $('#insects').show();
+      }  
+  });
+});
+
+// $(document).ready(function () {
+//   $("form#calculator").submit(function(event) {
+//     event.preventDefault();
+//       var number1 = parseInt($("#add1").val());
+//       var number2 = parseInt($("#add2").val());
+//       var operation = $("#operation").val();
+//       if(operation === "add"){
+//         result = add(number1, number2);
+//       } else if(operation === "subtract"){
+//         result = subtract(number1, number2)
+//       } else if(operation === "multiply"){
+//         result = multiply(number1, number2)
+//       } else if(operation === "divide"){
+//         result = divide(number1, number2)
+//       }
+//       var result;
+//       $("#output").text(result);
+//   });
+// });
